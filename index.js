a07d63e524a8f096bb01f12a9747c90cddbfb836
Richard Chen <richard.c.chen@gmail.com>
2018-06-25 12:16:04 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,10 +13,20 @@ function johnLennonFacts(array){
   var newarray = [];
   let i = 0;
   while (i < array.length){
-    console.log(array[i]);
+    //console.log(array[i]);
     newarray[i] = array[i] + excite;
-    console.log(newarray[i]);
+    //console.log(newarray[i]);
     i++;
   }
   return newarray;
+}
+
+function iLoveTheBeatles(number){
+  var newarray = [];
+  var love = "I love the Beatles!"
+  let i = 0;
+  do {
+    newarray[i] = love;
+    i++;
+  } while (number > 15)
 }
